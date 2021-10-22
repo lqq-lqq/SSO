2b6aa7959ae1da2be1221dd28be8cf07c1aed636
xxreal <1418798962@qq.com>
2021-10-22 23:48:55 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	sso-demo1/target/classes/com/sso/demo1/Demo1App.class
    #	sso-demo1/target/classes/com/sso/demo1/controller/ViewController.class

--- sso-login/src/main/java/com/sso/login/controller/LoginController.java
+++ sso-login/src/main/java/com/sso/login/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.sso.login.controller;
 
 import com.sso.login.com.sso.login.utils.LoginCacheUtils;
 import com.sso.login.domain.User;
+import com.sso.login.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,39 +11,40 @@ import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
 
 @Controller
 @RequestMapping(value = "/login",method = {RequestMethod.POST,RequestMethod.GET})
 public class LoginController {
-    private static Set<User> dbUsers;
-    static {
-        dbUsers=new HashSet<>();
-        dbUsers.add(new User("zhangsan","123456"));
-        dbUsers.add(new User("lisi","123456"));
-
-    }
+    @Autowired
+    private UserRepository userRepository;
     @PostMapping("")
-    public String doLogin(User user, HttpSession session, HttpServletResponse response){   //接受表单参数和重定向地址
+    public String doLogin(HttpServletRequest request,HttpSession session, HttpServletResponse response){   //接受表单参数和重定向地址
         String target=(String) session.getAttribute("target");  //拿到重定向的页面
         //模拟从数据库中通过用户名和密码查找用户
-        Optional<User> person=dbUsers.stream().filter(dbUser->dbUser.getUsername().equals(user.getUsername()) &&
-                dbUser.getPassword().equals(user.getPassword())).findFirst();
-        if(person.isPresent()){
-            //保存用户信息
-            String token= UUID.randomUUID().toString();
-            Cookie cookie=new Cookie("TOKEN",token);
-            cookie.setDomain("lqq.com"); //设置后缀域名
-            response.addCookie(cookie);
-            LoginCacheUtils.loginUser.put(token,person.get());
-        } else {  //登陆失败
-            session.setAttribute("msg","用户名或密码错误");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        User user = userRepository.findByUsername(username);
+        if(user==null){
+            session.setAttribute("msg","用户名不存在，请注册");
             return "index";
+        }
+        else{
+            if(password.equals(user.getPassword())){
+                String token= UUID.randomUUID().toString();
+                Cookie cookie=new Cookie("TOKEN",token);
+                cookie.setDomain("lqq.com"); //设置后缀域名
+                response.addCookie(cookie);
+                LoginCacheUtils.loginUser.put(token,user);
+                request.setAttribute("user",user);
+            }
+            else{
+                session.setAttribute("msg","用户名或密码错误");
+                return "index";
+            }
         }
         return "redirect:"+target;
     }
--- sso-login/src/main/java/com/sso/login/controller/SignUpController.java
+++ sso-login/src/main/java/com/sso/login/controller/SignUpController.java
@@ -1,5 +1,6 @@
 package com.sso.login.controller;
 
+import com.sso.login.domain.User;
 import com.sso.login.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,26 +19,27 @@ public class SignUpController {
     private UserRepository userRepository;
     @PostMapping("")
     public String toSign(HttpServletRequest request,HttpSession session){
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        System.out.println("aaaaaaaaaa  "+username);
-        System.out.println(password);
-//        //获取用户和密码
 //        String username = request.getParameter("username");
 //        String password = request.getParameter("password");
-//        //根据昵称查询用户是否存在
-//        User user = userRepository.findByUsername(username);
-//        //如果用户名存在
-//        if(user!=null){
-//            session.setAttribute("msg1","用户名重复");
-//            return "sign-up";
-//        }
-//        //如果用户名不存在
-//        User newUser = new User(username,password);
-//        //注册
-//        userRepository.save(newUser);
-//        //将信息设置session作用域
-//        request.setAttribute("user",newUser);
-        return "redirect:"+"http://login.lqq.com:8000/view/login";
+//        System.out.println("aaaaaaaaaa  "+username);
+//        System.out.println(password);
+        //获取用户和密码
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        //根据昵称查询用户是否存在
+        User user = userRepository.findByUsername(username);
+        //如果用户名存在
+        if(user!=null){
+            session.setAttribute("msg1","用户名重复");
+            return "sign-up";
+        }
+        //如果用户名不存在
+        User newUser = new User(username,password);
+        //注册
+        userRepository.save(newUser);
+        //将信息设置session作用域
+        request.setAttribute("user",newUser);
+        return("success");
+//        return "redirect:"+"http://login.lqq.com:8000/view/login";
     }
 }
